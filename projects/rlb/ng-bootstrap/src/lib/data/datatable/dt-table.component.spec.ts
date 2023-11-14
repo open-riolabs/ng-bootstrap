2ac4e7c4b3c3cff50a6d34f5abf13919cba0cbdc
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-11-14 21:00:12 +0100

    rm test dt

--- projects/rlb/ng-bootstrap/src/lib/data/datatable/dt-table.component.spec.ts
+++ projects/rlb/ng-bootstrap/src/lib/data/datatable/dt-table.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DataTableComponent } from './dt-table.component';
+// import { DataTableComponent } from './dt-table.component';
 
-describe('DataTableComponent', () => {
-  let component: DataTableComponent;
-  let fixture: ComponentFixture<DataTableComponent>;
+// describe('DataTableComponent', () => {
+//   let component: DataTableComponent;
+//   let fixture: ComponentFixture<DataTableComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DataTableComponent]
-    });
-    fixture = TestBed.createComponent(DataTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [DataTableComponent]
+//     });
+//     fixture = TestBed.createComponent(DataTableComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
