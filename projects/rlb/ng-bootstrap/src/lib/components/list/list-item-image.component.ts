04793e8f888b4ff7d8be41cab96c72200154c62b
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2024-01-06 15:13:05 +0100

    added click in list item

--- projects/rlb/ng-bootstrap/src/lib/components/list/list-item-image.component.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/list/list-item-image.component.ts
@@ -1,34 +1,43 @@
-import { Component, Input, TemplateRef, ViewChild, ViewContainerRef, booleanAttribute } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+  booleanAttribute,
+} from '@angular/core';
 
 @Component({
   selector: 'rlb-list-item-image',
-  template: `
-  <ng-template #template>
-    <li class="list-group-item"
-      [class.disabled]="disabled" 
-      [class.list-group-item-action]="disabled !== true" 
+  template: ` <ng-template #template>
+    <li
+      class="list-group-item"
+      [class.disabled]="disabled"
+      [class.list-group-item-action]="disabled !== true"
       [class.active]="active"
-      [attr.aria-current]="active">
+      [attr.aria-current]="active"
+      (click)="click.emit($event)"
+    >
       <div class="d-flex">
         <rlb-avatar [src]="profilePicture" [size]="avatarSize" />
         <div class="ps-2 flex-grow-1">
-          <div class="fw-bold">{{username}}</div>
-          <span *ngIf="line1" class="d-block">{{line1}}</span>
-          <span *ngIf="line2" class="d-block">{{line2}}</span>
+          <div class="fw-bold">{{ username }}</div>
+          <span *ngIf="line1" class="d-block">{{ line1 }}</span>
+          <span *ngIf="line2" class="d-block">{{ line2 }}</span>
         </div>
         <div *ngIf="unread">
-          <span rlb-badge [pill]="true">{{unread}}</span>
+          <span rlb-badge [pill]="true">{{ unread }}</span>
         </div>
       </div>
     </li>
   </ng-template>`,
-  host: {
-
-  },
+  host: {},
 })
 export class ListItemImageComponent {
-  @Input({ transform: booleanAttribute, alias: 'active' }) active!: boolean
-  @Input({ transform: booleanAttribute, alias: 'disabled' }) disabled!: boolean
+  @Input({ transform: booleanAttribute, alias: 'active' }) active!: boolean;
+  @Input({ transform: booleanAttribute, alias: 'disabled' }) disabled!: boolean;
   @Input('avatar-size') avatarSize?: number = 50;
   @Input('username') username?: string;
   @Input('line-1') line1?: string;
@@ -36,13 +45,16 @@ export class ListItemImageComponent {
   @Input('profile-picture') profilePicture?: string;
   @Input('unread') unread?: number | string;
   @ViewChild('template', { static: true }) template!: TemplateRef<any>;
+  @Output() click = new EventEmitter<MouseEvent>();
   element!: HTMLElement;
 
-  constructor(private viewContainerRef: ViewContainerRef) { }
+  constructor(private viewContainerRef: ViewContainerRef) {}
 
   ngOnInit() {
-    const templateView = this.viewContainerRef.createEmbeddedView(this.template);
-    this.element = (templateView.rootNodes[0]);
+    const templateView = this.viewContainerRef.createEmbeddedView(
+      this.template
+    );
+    this.element = templateView.rootNodes[0];
     this.viewContainerRef.element.nativeElement.remove();
   }
 }
--- projects/rlb/ng-bootstrap/src/lib/components/list/list-item.component.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/list/list-item.component.ts
@@ -1,34 +1,45 @@
-import { Component, Input, TemplateRef, ViewChild, ViewContainerRef, booleanAttribute } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+  booleanAttribute,
+} from '@angular/core';
 
 @Component({
   selector: 'rlb-list-item',
-  template: `
-  <ng-template #template>
-    <li class="list-group-item"
-      [class.disabled]="disabled" 
-      [class.list-group-item-action]="action" 
+  template: ` <ng-template #template>
+    <li
+      class="list-group-item"
+      [class.disabled]="disabled"
+      [class.list-group-item-action]="action"
       [class.active]="active"
-      [attr.aria-current]="active">
+      [attr.aria-current]="active"
+      (click)="click.emit($event)"
+    >
       <ng-content></ng-content>
     </li>
   </ng-template>`,
-  host: {
-
-  },
+  host: {},
 })
 export class ListItemComponent {
-  @Input({ transform: booleanAttribute, alias: 'active' }) active?: boolean
-  @Input({ transform: booleanAttribute, alias: 'disabled' }) disabled?: boolean
-  @Input({ transform: booleanAttribute, alias: 'action' }) action?: boolean
+  @Input({ transform: booleanAttribute, alias: 'active' }) active?: boolean;
+  @Input({ transform: booleanAttribute, alias: 'disabled' }) disabled?: boolean;
+  @Input({ transform: booleanAttribute, alias: 'action' }) action?: boolean;
+  @Output() click = new EventEmitter<MouseEvent>();
 
   @ViewChild('template', { static: true }) template!: TemplateRef<any>;
   element!: HTMLElement;
 
-  constructor(private viewContainerRef: ViewContainerRef) {
-  }
+  constructor(private viewContainerRef: ViewContainerRef) {}
   ngOnInit() {
-    const templateView = this.viewContainerRef.createEmbeddedView(this.template);
-    this.element = (templateView.rootNodes[0]);
+    const templateView = this.viewContainerRef.createEmbeddedView(
+      this.template
+    );
+    this.element = templateView.rootNodes[0];
     this.viewContainerRef.element.nativeElement.remove();
   }
 }
