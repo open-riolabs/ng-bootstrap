7264f011b419d66296ab4eec8211e9cf05ef4d7f
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2024-04-15 12:04:41 +0200

    .

--- projects/rlb/ng-bootstrap/src/public-api.ts
+++ projects/rlb/ng-bootstrap/src/public-api.ts
@@ -13,6 +13,7 @@ export * from './lib/data/datatable';
 export * from './lib/forms/inputs';
 export * from './lib/forms/rlb-form-fields/rlb-form-fields.component';
 
+export * from './lib/shared/unique-id.service';
 export * from './lib/shared/types';
 export * from './lib/interfaces';
 export * from './lib/modals';
