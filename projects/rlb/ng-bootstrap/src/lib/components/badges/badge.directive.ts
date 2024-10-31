6c3e96868b07ff6bdf597715f03baf714d79910c
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2024-10-31 15:28:19 +0100

    released fix badge

--- projects/rlb/ng-bootstrap/src/lib/components/badges/badge.directive.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/badges/badge.directive.ts
@@ -6,65 +6,84 @@ import {
   DoCheck,
   booleanAttribute,
   numberAttribute,
+  AfterViewChecked,
+  AfterViewInit,
 } from '@angular/core';
 import { Color } from '../../shared/types';
 
 @Directive({
   selector: '[badge]',
 })
-export class BadgeDirective implements DoCheck {
+export class BadgeDirective implements AfterViewInit, DoCheck {
   @Input({ alias: 'badge' }) badge?: string;
   @Input({ alias: 'badge-pill', transform: booleanAttribute }) pill!: boolean;
-  @Input({ alias: 'badge-border', transform: booleanAttribute }) border!: boolean
+  @Input({ alias: 'badge-border', transform: booleanAttribute }) border!: boolean;
   @Input({ alias: 'badge-top', transform: numberAttribute }) top!: number;
   @Input({ alias: 'badge-start', transform: numberAttribute }) start!: number;
   @Input({ alias: 'badge-color' }) color: Color = 'danger';
   @Input({ alias: 'hidden-text' }) hiddenText!: string;
+
+  private badgeContent!: any;
+  private badgeElement!: HTMLElement;
 
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2,
   ) { }
 
-  ngDoCheck() {
-    const badge = this.renderer.createElement('span');
+  ngDoCheck(): void {
+    if (this.badge) {
+      if (this.badgeContent) {
+        this.badgeContent.data = this.badge;
+        this.badgeElement.style.display = 'inline';
+      }
+    } else {
+      if (this.badgeContent) {
+        this.badgeElement.style.display = 'none';
+      }
+    }
+  }
+
+  ngAfterViewInit() {
+    this.badgeElement = this.renderer.createElement('span');
 
     if (this.top || this.start || this.top === 0 || this.start === 0) {
-      this.renderer.addClass(badge, 'position-absolute');
+      this.renderer.addClass(this.badgeElement, 'position-absolute');
       if (this.top || this.top === 0) {
-        this.renderer.addClass(badge, `top-${this.top}`);
+        this.renderer.addClass(this.badgeElement, `top-${this.top}`);
       }
       if (this.start || this.start === 0) {
-        this.renderer.addClass(badge, `start-${this.start}`);
+        this.renderer.addClass(this.badgeElement, `start-${this.start}`);
       }
-      this.renderer.addClass(badge, 'translate-middle');
+      this.renderer.addClass(this.badgeElement, 'translate-middle');
     }
-    this.renderer.addClass(badge, 'badge');
+    this.renderer.addClass(this.badgeElement, 'badge');
     if (this.pill) {
-      this.renderer.addClass(badge, 'rounded-pill');
+      this.renderer.addClass(this.badgeElement, 'rounded-pill');
     }
     if (this.border) {
-      this.renderer.addClass(badge, 'rounded-border');
+      this.renderer.addClass(this.badgeElement, 'rounded-border');
     }
     if (this.color) {
-      this.renderer.addClass(badge, `bg-${this.color}`);
+      this.renderer.addClass(this.badgeElement, `bg-${this.color}`);
     }
     if (this.badge) {
-      this.renderer.appendChild(badge, this.renderer.createText(this.badge));
+      this.badgeContent = this.renderer.createText(this.badge);
+      this.renderer.appendChild(this.badgeElement, this.badgeContent);
     } else {
       if (this.top || this.start || this.top === 0 || this.start === 0) {
-        this.renderer.addClass(badge, `p-2`);
+        this.renderer.addClass(this.badgeElement, `p-2`);
       } else {
-        this.renderer.addClass(badge, `ps-0`);
-        this.renderer.addClass(badge, `ms-2`);
+        this.renderer.addClass(this.badgeElement, `ps-0`);
+        this.renderer.addClass(this.badgeElement, `ms-2`);
       }
-      this.renderer.addClass(badge, `border`);
-      this.renderer.addClass(badge, `border-light`);
-      this.renderer.addClass(badge, `rounded-circle`);
+      this.renderer.addClass(this.badgeElement, `border`);
+      this.renderer.addClass(this.badgeElement, `border-light`);
+      this.renderer.addClass(this.badgeElement, `rounded-circle`);
       if (!this.hiddenText) {
         const text = this.renderer.createElement('span');
         this.renderer.addClass(text, 'visually-hidden');
-        this.renderer.appendChild(badge, text);
+        this.renderer.appendChild(this.badgeElement, text);
       }
     }
     if (this.hiddenText) {
@@ -74,8 +93,8 @@ export class BadgeDirective implements DoCheck {
         text,
         this.renderer.createText(this.hiddenText),
       );
-      this.renderer.appendChild(badge, text);
+      this.renderer.appendChild(this.badgeElement, text);
     }
-    this.renderer.appendChild(this.elementRef.nativeElement, badge);
+    this.renderer.appendChild(this.elementRef.nativeElement, this.badgeElement);
   }
 }
