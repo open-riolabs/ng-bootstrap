607058691ddbb140fd380ab976a756e304e22a4e
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2025-06-27 15:18:57 +0200

    Refactor sidebar component styles and improve unique ID handling in sidebar items

--- projects/rlb/ng-bootstrap/src/lib/components/sidebar/sidebar-item.component.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/sidebar/sidebar-item.component.ts
@@ -1,4 +1,6 @@
-import { booleanAttribute, Component, ContentChildren, Input, OnInit, QueryList, TemplateRef, ViewChild, ViewContainerRef } from "@angular/core";
+import { Component, ContentChildren, Input, OnInit, QueryList, TemplateRef, ViewChild, ViewContainerRef } from "@angular/core";
+import { UniqueIdService } from "../../shared/unique-id.service";
+
 
 @Component({
   selector: 'rlb-sidebar-item',
@@ -6,13 +8,15 @@ import { booleanAttribute, Component, ContentChildren, Input, OnInit, QueryList,
     <ng-template #template>
       <li *ngIf="title" class="menu-title">{{ title }}</li>
       <li *ngIf="!title">
-        <a *ngIf="children?.length" href="javascript:void(0);" class="is-parent has-arrow">
+        <a *ngIf="children?.length" href="javascript:void(0);" class="is-parent has-arrow" toggle="collapse" [toggle-target]="'side-item' + _id">
           <i *ngIf="icon" [class]="icon"></i>
           <span>{{ label }}</span>
         </a>
-        <ul class="sub-menu" aria-expanded="false">
-          <ng-content select="rlb-sidebar-item"></ng-content>
-        </ul>
+        <rlb-collapse [id]="'side-item' + _id">
+          <ul class="sub-menu" aria-expanded="false">
+            <ng-content select="rlb-sidebar-item"></ng-content>
+          </ul>
+        </rlb-collapse>
         <a *ngIf="!children?.length" [routerLink]="link" class="side-nav-link-ref" routerLinkActive="active">
           <i *ngIf="icon" [class]="icon"></i>
           <ng-content></ng-content>
@@ -32,7 +36,12 @@ export class SidebarItemComponent implements OnInit {
   @ContentChildren(SidebarItemComponent) children!: QueryList<SidebarItemComponent>;
   @ViewChild('template', { static: true }) template!: TemplateRef<any>;
 
-  constructor(private viewContainerRef: ViewContainerRef) { }
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private uniqueIdService: UniqueIdService
+  ) { }
+
+  _id: string = '';
 
   ngOnInit() {
     const templateView = this.viewContainerRef.createEmbeddedView(
@@ -40,6 +49,6 @@ export class SidebarItemComponent implements OnInit {
     );
     this.element = templateView.rootNodes[0];
     this.viewContainerRef.element.nativeElement.remove();
+    this._id = this.uniqueIdService.id;
   }
-
 }
