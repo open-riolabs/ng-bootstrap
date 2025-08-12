bd41ba801ea185fe0cd4dee3f05b6e1e50ee8853
Denis <neorimne@gmail.com>
2025-08-12 11:45:13 +0200

    Add API description autocomplete component

--- projects/rlb/ng-bootstrap/src/lib/forms/inputs/autocomplete.component.ts
+++ projects/rlb/ng-bootstrap/src/lib/forms/inputs/autocomplete.component.ts
@@ -56,6 +56,9 @@ export class AutocompleteComponent
 
   @Input({ transform: booleanAttribute, alias: 'disabled' }) disabled? = false;
   @Input({ transform: booleanAttribute, alias: 'readonly' }) readonly? = false;
+  // TODO
+  // What should this input do?
+  // Currently not used
   @Input({ transform: booleanAttribute, alias: 'before-text' }) beforeText?: boolean = false;
   @Input({ transform: booleanAttribute, alias: 'loading' }) loading?: boolean = false;
   @Input({ transform: numberAttribute, alias: 'max-height' }) maxHeight?: number = 200;
