cb0eb73c484f9e999d50637efbac587ae0b29094
Denis <neorimne@gmail.com>
2025-08-12 15:28:01 +0200

    Add API description input-group component. Add formControls in InputGroupsComponent for explicit example of group validation

--- src/app/pages/inputs/input-group/input-group.component.ts
+++ src/app/pages/inputs/input-group/input-group.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-input-group',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
 })
 export class InputGroupsComponent {
 
-  html: string = `<rlb-input-group>text</rlb-input-group>`;
+	html: string = `
+<rlb-input-group validate>
+  <rlb-input name="email" [formControl]="emailControl" placeholder="Email"></rlb-input>
+  <rlb-input name="phone" [formControl]="phoneControl" placeholder="Phone"></rlb-input>
+  <rlb-input-validation></rlb-input-validation>
+</rlb-input-group>`;
+
+	ts = `
+@Component({
+selector: 'app-alerts',
+templateUrl: './alerts.component.html',
+ styleUrls: ['./alerts.component.scss'],
+})
+export class AlertsComponent {
+    emailControl = new FormControl('', [Validators.required]);
+	phoneControl = new FormControl('', [Validators.required]);
+}`
+
+	emailControl = new FormControl('', [Validators.required]);
+	phoneControl = new FormControl('', [Validators.required]);
+
 }
