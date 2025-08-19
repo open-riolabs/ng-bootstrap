f4e4e674ead9939bca85e73d614b67e093aff161
Denis <neorimne@gmail.com>
2025-08-19 16:52:36 +0200

    refactor(lib nav component): code cleanup, minor bugfix
    docs(nav component): edit documentation page and API description

--- src/app/pages/components/navigations/navs/nav.component.ts
+++ src/app/pages/components/navigations/navs/nav.component.ts
@@ -8,18 +8,18 @@ import { Component } from '@angular/core';
 export class NavsComponent {
 
   html: string = `<rlb-nav id="mainNav" horizontal-alignment="center" vertical="false" fill="true" pills="true" class="custom-nav-class">
-  <rlb-nav-item>
-    <a class="nav-link active" href="#home">Home</a>
-  </rlb-nav-item>
-  <rlb-nav-item>
-    <a class="nav-link" href="#about">About</a>
-  </rlb-nav-item>
-  <rlb-nav-item>
-    <a class="nav-link" href="#services">Services</a>
-  </rlb-nav-item>
-  <rlb-nav-item>
-    <a class="nav-link" href="#contact">Contact</a>
-  </rlb-nav-item>
+ <rlb-nav-item href="#1">
+  Home
+ </rlb-nav-item>
+ <rlb-nav-item href="#2">
+  About
+ </rlb-nav-item>
+ <rlb-nav-item href="#3">
+  Services
+ </rlb-nav-item>
+ <rlb-nav-item href="#4">
+  Contact
+ </rlb-nav-item>
 </rlb-nav>`;
 
   ts: string = `@Component({
