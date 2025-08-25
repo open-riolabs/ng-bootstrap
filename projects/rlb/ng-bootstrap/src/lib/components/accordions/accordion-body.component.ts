2ff5619a3d55ed1c72d540138b227ad98b5c5960
Denis <neorimne@gmail.com>
2025-08-25 17:06:05 +0200

    fix(AccordionBodyComponent): delete redundant attribute binding

--- projects/rlb/ng-bootstrap/src/lib/components/accordions/accordion-body.component.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/accordions/accordion-body.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
     host: {
         class: 'accordion-collapse collapse',
         '[class.show]': 'expanded',
-        '[data-bs-parent]': 'parentId',
         '[id]': 'itemId',
         '[attr.data-bs-parent]': "'#'+parentId",
     },
