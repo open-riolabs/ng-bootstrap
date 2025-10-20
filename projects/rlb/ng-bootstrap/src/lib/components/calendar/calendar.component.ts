9841839af7254bf1e9c20c3ee6227cc50a035775
Denis <neorimne@gmail.com>
2025-10-20 16:52:44 +0200

    refactor: (CalendarComponent) implemented timeline and navigate buttons

--- projects/rlb/ng-bootstrap/src/lib/components/calendar/calendar.component.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, Component, EventEmitter, Input, Output } from "@angular/core";
+import { booleanAttribute, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from "@angular/core";
 import { CalendarEvent } from "./interfaces/calendar-event.interface";
 import { DateTz, IDateTz } from "@open-rlb/date-tz";
 import { CalendarView } from "./interfaces/calendar-view.type";
@@ -9,7 +9,7 @@ import { CalendarView } from "./interfaces/calendar-view.type";
 	styleUrls: ["./calendar.component.scss"],
 	standalone: false
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnChanges {
 	
 	@Input({ alias: 'view' }) view: CalendarView = 'week';
 	
@@ -25,12 +25,32 @@ export class CalendarComponent {
 	@Output('view-change') viewChange = new EventEmitter<'month' | 'week' | 'day'>();
 	@Output('event-click') eventClick = new EventEmitter<CalendarEvent>();
 	
+	private readonly userTimeZone: string;
+	
 	constructor() {
+		this.userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+		this.currentDate = DateTz.now(this.userTimeZone);
 	}
 	
-	next() {   }
-	prev() {   }
-	today() {   }
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes['currentDate']) {
+			const incomingDate = changes['currentDate'].currentValue as IDateTz;
+			if (incomingDate && incomingDate.timezone !== this.userTimeZone) {
+				this.currentDate = new DateTz(incomingDate.timestamp, incomingDate.timezone).convertToTimezone(this.userTimeZone);
+			} else if (incomingDate) {
+				this.currentDate = incomingDate;
+			} else {
+				this.currentDate = DateTz.now(this.userTimeZone);
+			}
+		}
+	}
+	
+	
+	setDate(date: IDateTz) {
+		this.currentDate = date;
+		this.dateChange.emit(date)
+	}
+	
 	setView(view: CalendarView) {
 		this.view = view;
 		this.viewChange.emit(view)
