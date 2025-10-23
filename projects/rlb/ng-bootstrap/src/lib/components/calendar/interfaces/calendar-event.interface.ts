802253fb344db06adf7c62c382934c1de4eea4a8
Denis <neorimne@gmail.com>
2025-10-23 09:39:37 +0200

    refactor: interface update

--- projects/rlb/ng-bootstrap/src/lib/components/calendar/interfaces/calendar-event.interface.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/calendar/interfaces/calendar-event.interface.ts
@@ -1,12 +1,12 @@
 import { Color } from "../../../shared/types";
 import { IDateTz } from "@open-rlb/date-tz";
 
-export interface CalendarEvent {
+export interface CalendarEvent<T = any> {
 	id?: string | number;
 	title: string;
 	start: IDateTz;
 	end: IDateTz;
 	color?: Color;
 	allDay?: boolean;
-	data?: any;
+	data?: T;
 }
