d0dcc728b6120019ef91b450c5d2ed485271d82d
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2026-06-10 23:15:31 +0200

    .

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { DateTz, IDateTz } from '@open-rlb/date-tz';
-import {
-  AutocompleteItem,
-  ModalService,
-  requiredAutocompleteValue,
-  ToastService,
-} from 'projects/rlb/ng-bootstrap/src/public-api';
 
 import { SHARED_IMPORTS } from '../../shared-imports';
 
@@ -17,59 +9,12 @@ import { SHARED_IMPORTS } from '../../shared-imports';
   imports: [SHARED_IMPORTS],
 })
 export class HomeComponent {
-  number: number = 0;
-  dtz: IDateTz = DateTz.now();
-  str: string = 'Hello World!';
-  timezone: string = '';
-  country: string = '';
-  carouselPage: number = 0;
-  carouselCount: number = 0;
-  countryDialCode = '';
+  installCmd = 'ng add @open-rlb/ng-bootstrap';
+  copied = false;
 
-  profileForm = new FormGroup({
-    firstName: new FormControl<string>('dd', [Validators.required, Validators.minLength(2)]),
-    country: new FormControl<AutocompleteItem>({ text: '', value: '' }, [
-      requiredAutocompleteValue(),
-    ]),
-    profile: new FormGroup({
-      culture: new FormGroup({
-        timezone: new FormControl<string>('', [requiredAutocompleteValue()]),
-      }),
-    }),
-    dialCode: new FormControl<AutocompleteItem>({ text: '', value: '' }, [
-      requiredAutocompleteValue(),
-    ]),
-  });
-
-  constructor(
-    private modals: ModalService,
-    private toasts: ToastService,
-  ) {
-    this.profileForm.valueChanges.subscribe(value => {
-      console.log('Form value: ', value);
-    });
-    this.profileForm.patchValue({
-      profile: {
-        culture: {
-          timezone: 'Europe/Rome',
-        },
-      },
-    });
-  }
-
-  updateProfile() {}
-
-  test() {
-    console.log('Form value: ', this.profileForm.value);
-  }
-
-  testPatch() {
-    this.profileForm.patchValue({
-      profile: {
-        culture: {
-          timezone: 'Europe/Paris' as any,
-        },
-      },
-    });
+  copy() {
+    navigator.clipboard?.writeText(this.installCmd);
+    this.copied = true;
+    setTimeout(() => (this.copied = false), 1500);
   }
 }
